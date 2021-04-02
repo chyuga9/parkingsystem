bec760ca9367ba0316cc24a8e340c4c0b725b44f
Chyuga <Chyuga@DESKTOP-IDEC152>
2021-04-02 21:23:16 +0200

    Ajout de l'ordre décroissant dans GET_TICKET_TEST

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -8,7 +8,7 @@ public class DBConstants {
     public static final String SAVE_TICKET = "insert into ticket(PARKING_NUMBER, VEHICLE_REG_NUMBER,IN_TIME, OUT_TIME, RECURRING_VEHICLE) values(?,?,?,?,?)";
     public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=? where ID=?";
     public static final String GET_TICKET = "select t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, t.RECURRING_VEHICLE, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME DESC limit 1";
-    public static final String GET_TICKET_TEST = "select t.VEHICLE_REG_NUMBER, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME  limit 1";
+    public static final String GET_TICKET_TEST = "select t.VEHICLE_REG_NUMBER, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME DESC limit 1";
     public static final String UPDATE_TICKET_TEST = "select t.PRICE, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME  limit 1";
     public static final String UPDATE_IN_TIME_TICKET_TEST = "update ticket set IN_TIME=? where ID=?";
     
